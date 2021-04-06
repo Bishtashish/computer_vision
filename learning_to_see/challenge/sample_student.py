a8925de8d6008ad59a82321bf913d5215013e7f1
Bishtashish <ashishauthenticash@gmail.com>
2021-04-06 11:50:48 -0400

    Learning to see

---
+++ learning_to_see/challenge/sample_student.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+def breakIntoGrids(im, s = 9):
+    grids = []
+    h = s//2 #half grid size minus one.
+    for i in range(h, im.shape[0]-h):
+        for j in range(h, im.shape[1]-h):
+            grids.append(im[i-h:i+h+1,j-h:j+h+1].ravel())
+    return np.vstack(grids)
+
+def reshapeIntoImage(vector, im_shape, s = 9):
+    h = s//2 #half grid size minus one. 
+    image = np.zeros(im_shape)
+    image[h:-h, h:-h] = vector.reshape(im_shape[0]-2*h, im_shape[1]-2*h)
+    return image
+
+labels = [1, 2, 3]
+def countEdges(splicedRow):
+    count=0
+    for i in range(len(splicedRow)):
+        if i==0:
+            if splicedRow[i]==True:
+                count=count+1   
+        else:
+            if splicedRow[i-1]==False and splicedRow[i]==True:
+                count=count+1
+    return count
+
+def count_fingers(im):
+    im = im > 94 #Threshold image
+    X = breakIntoGrids(im, s = 9) #Break into 9x9 grids
+    #Use rule we learned with decision tree
+    treeRule1 = lambda X: np.logical_and(np.logical_and(X[:, 40] == 1, X[:,0] == 0), X[:, 53] == 0)
+#     treeRule1 = lambda X: np.logical_and(np.logical_and(X[:, 13] == 1, X[:,0] == 0), X[:, 16] == 0)
+    yhat = treeRule1(X)
+    yhat_reshaped = reshapeIntoImage(yhat, im.shape, s=9)
+    splicedRow = yhat_reshaped[yhat_reshaped.shape[0]//4,:]
+    bool_list = list(map(bool,splicedRow))
+#     return countEdges(splicedRow.tolist())
+    count = sum([1 if (i ==0 and splicedRow[i] == True ) or (splicedRow[i-1]==False and splicedRow[i]==True) else 0 for i,n in enumerate(bool_list)])
+    #count= [sum(1) for i,n in enumerate(bool_list) if(n==True and bool_list[i-1]==False)]
+    print(count)
+    return count
+    
